f7151926f677c838140fdba7ac0c591630c59760
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2023-09-13 15:41:52 +0200

    création de la polyligne

--- main.ts
+++ main.ts
@@ -1,14 +1,17 @@
+import proj4 from 'proj4';
+
 import Map from 'ol/Map.js';
 import OSM from 'ol/source/OSM.js';
 import TileLayer from 'ol/layer/Tile.js';
 import View from 'ol/View.js';
-import proj4 from 'proj4';
 import { register } from 'ol/proj/proj4';
-import { Point } from 'ol/geom';
+import { LineString, Point } from 'ol/geom';
 import { Feature } from 'ol';
 import VectorSource from 'ol/source/Vector';
 import VectorLayer from 'ol/layer/Vector';
 import { Style, Circle, Fill, Stroke } from 'ol/style';
+import { buffer } from 'ol/extent';
+import { Coordinate } from 'ol/coordinate';
 
 proj4.defs("EPSG:31370","+proj=lcc +lat_0=90 +lon_0=4.36748666666667 +lat_1=51.1666672333333 +lat_2=49.8333339 +x_0=150000.013 +y_0=5400088.438 +ellps=intl +towgs84=-106.8686,52.2978,-103.7239,-0.3366,0.457,-1.8422,-1.2747 +units=m +no_defs +type=crs");
 
@@ -26,7 +29,7 @@ const map = new Map({
   view: new View({
     projection: 'EPSG:31370',
     center: grandPlace,
-    zoom: 16,
+    zoom: 2,
   }),
 });
 
@@ -49,17 +52,37 @@ const pointLayer = new VectorLayer({
 
 map.addLayer(pointLayer);
 
+const lineSource = new VectorSource();
+
+const lineLayer = new VectorLayer({
+  source: lineSource,
+});
+
+map.addLayer(lineLayer);
+
 fetch('/ambulance1.json')
     .then(async response => {
         const file = await response.json();
         const data = file.data;
+        const coordinates: Coordinate[] = [];
 
         for (const item of data) {
-          const x = item.x;
-          const y = item.y;
+          const x = Number.parseFloat(item.x);
+          const y = Number.parseFloat(item.y);
 
+          coordinates.push([x, y]);
           const ambulancePoint = new Point([x, y]);
           const ambulanceFeature = new Feature({ geometry: ambulancePoint });
           pointSource.addFeature(ambulanceFeature);
         }
+
+        const polyLine = new LineString(coordinates);
+        const trajet = new Feature({ geometry: polyLine });
+
+        lineSource.addFeature(trajet);
+
+        const extent = pointSource.getExtent();
+        const bufferedExtent = buffer(extent, 500);
+
+        map.getView().fit(bufferedExtent);
     })
