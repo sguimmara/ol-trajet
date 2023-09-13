44b1d1e987f182c5c3d1239f76fc82a4005fd7b3
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2023-09-13 16:07:26 +0200

    coloration en fonction du status

--- main.ts
+++ main.ts
@@ -12,6 +12,7 @@ import VectorLayer from 'ol/layer/Vector';
 import { Style, Circle, Fill, Stroke } from 'ol/style';
 import { buffer } from 'ol/extent';
 import { Coordinate } from 'ol/coordinate';
+import { FeatureLike } from 'ol/Feature';
 
 proj4.defs("EPSG:31370","+proj=lcc +lat_0=90 +lon_0=4.36748666666667 +lat_1=51.1666672333333 +lat_2=49.8333339 +x_0=150000.013 +y_0=5400088.438 +ellps=intl +towgs84=-106.8686,52.2978,-103.7239,-0.3366,0.457,-1.8422,-1.2747 +units=m +no_defs +type=crs");
 
@@ -33,14 +34,29 @@ const map = new Map({
   }),
 });
 
-const pointSource = new VectorSource();
-const pointLayer = new VectorLayer({
-  source: pointSource,
-  style: new Style({
+function getColorFromStatus(status: number) {
+  switch (status) {
+    case 0: return 'blue';
+    case 1: return 'green';
+    case 2: return 'yellow';
+    case 3: return 'orange';
+    case 4: return 'cyan';
+    case 5: return 'gray';
+    case 6: return 'white';
+    case 7: return 'black';
+  }
+}
+
+function getStyleFromStatus(feature: FeatureLike) {
+  const statusText = feature.get('status');
+  const status = Number.parseInt(statusText);
+  const fillColor = getColorFromStatus(status);
+
+  return new Style({
     image: new Circle({
       radius: 5,
       fill: new Fill({
-        color: 'yellow',
+        color: fillColor,
       }),
       stroke: new Stroke({
         color: 'black',
@@ -48,9 +64,13 @@ const pointLayer = new VectorLayer({
       })
     })
   })
-});
+}
 
-map.addLayer(pointLayer);
+const pointSource = new VectorSource();
+const pointLayer = new VectorLayer({
+  source: pointSource,
+  style: getStyleFromStatus,
+});
 
 const lineSource = new VectorSource();
 
@@ -59,6 +79,7 @@ const lineLayer = new VectorLayer({
 });
 
 map.addLayer(lineLayer);
+map.addLayer(pointLayer);
 
 fetch('/ambulance1.json')
     .then(async response => {
@@ -72,13 +93,28 @@ fetch('/ambulance1.json')
 
           coordinates.push([x, y]);
           const ambulancePoint = new Point([x, y]);
+
+          // "timedate": "2022-09-06 09:50:40",
+          // "status": "2",
+          // "speed": "0"
+
+          const speed = item.speed;
+          const status = item.status;
+          const timedate = item.timedate;
+
           const ambulanceFeature = new Feature({ geometry: ambulancePoint });
+
+          ambulanceFeature.set('speed', speed);
+          ambulanceFeature.set('status', status);
+          ambulanceFeature.set('timedate', timedate);
+
           pointSource.addFeature(ambulanceFeature);
         }
 
         const polyLine = new LineString(coordinates);
         const trajet = new Feature({ geometry: polyLine });
 
+        // On ajoute la ligne à la source
         lineSource.addFeature(trajet);
 
         const extent = pointSource.getExtent();
