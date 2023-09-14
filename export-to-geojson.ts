dc3dafc7917cd6c5fdca51c4a2a4a692d535780f
Sébastien Guimmara <sebastien.guimmara@oslandia.com>
2023-09-14 16:11:51 +0200

    export to geojson

--- export-to-geojson.ts
+++ export-to-geojson.ts
@@ -1,7 +1,12 @@
-import { Map, View } from "ol";
+import { Feature, Map, View } from "ol";
 import TileLayer from "ol/layer/Tile";
 import { fromLonLat } from "ol/proj";
 import OSM from 'ol/source/OSM';
+import Draw from 'ol/interaction/Draw';
+import VectorSource from "ol/source/Vector";
+import VectorLayer from "ol/layer/Vector";
+import { Circle, Fill, Stroke, Style } from "ol/style";
+import GeoJSON from 'ol/format/GeoJSON';
 
 const osm = new TileLayer({
     source: new OSM(),
@@ -15,3 +20,47 @@ const map = new Map({
         zoom: 15,
     })
 })
+
+const source = new VectorSource();
+
+const vectorLayer = new VectorLayer({
+    source,
+    style: new Style({
+        image: new Circle({
+            radius: 10,
+            fill: new Fill({
+                color: 'red',
+            }),
+            stroke: new Stroke({
+                color: 'black',
+            })
+        })
+    })
+});
+
+map.addLayer(vectorLayer);
+
+const draw = new Draw({
+    type: 'Point',
+    source,
+})
+
+map.addInteraction(draw);
+
+const button = document.getElementById('export') as HTMLButtonElement;
+
+button.onclick = function exportFeatures() {
+    const encoder = new GeoJSON({
+        featureProjection: 'EPSG:3857',
+        dataProjection: 'EPSG:4326',
+    });
+
+    const geojson = encoder.writeFeatures(source.getFeatures());
+
+    console.log(geojson);
+
+    fetch('http://belgique.com', {
+        method: 'POST',
+        body: new TextEncoder().encode(geojson),
+    })
+}
